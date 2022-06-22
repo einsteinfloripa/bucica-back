4f6d4711dffd9a3054e9154a2634340aaeaa9a29
DjonysDalmy <djonysdalmy@gmail.com>
2022-06-22 17:53:06 -0300

    resolve cors

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+header('Content-type: application/json; charset=utf-8');
+
 require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-header('Content-type: application/json; charset=utf-8');
 $URL = explode("/",$_SERVER[ 'REQUEST_URI' ]);
 $INPUT =  json_decode(file_get_contents('php://input'), TRUE);
 
